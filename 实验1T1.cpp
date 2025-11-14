f8ca13c0f8397303aa494a63055497722e8c0b70
liqingxu180 <8209250206@csu.edu.cn>
2025-11-14 13:09:35 +0800

    Add basic C++ program with output statements

--- 实验1T1.cpp
+++ 实验1T1.cpp
@@ -1,14 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+1.
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int k = 5;
+    int i = k + 1;
+    cout << i++ << endl;
+    i = 1;
+    cout << i++ << endl;
+    cout << "Welcome to C++" << endl;
+    return 0;
+}
